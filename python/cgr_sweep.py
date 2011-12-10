93ff8c7e0dcd1ca36d8a29ce84cc81084dc0376b
John Peck <sunnyvalejp@gmail.com>
2011-12-10 12:47:04 -0800

    Modified cgr_sweep to use serial instead of visa.

--- python/cgr_sweep.py
+++ python/cgr_sweep.py
@@ -3,14 +3,15 @@
 from numpy import *
 import Gnuplot, Gnuplot.funcutils
 import socket   # For TCP/IP sockets
-import visa     # For GPIB or RS-232
+# import visa     # For GPIB or RS-232
+import serial   # From pyserial package
 import time     # For making pauses
 import os       # For basic file I/O
 
 # ------------------------- Begin configure ----------------------------
-cgrch = 6       # COM port for CGR-101
-baud = 230400   # Baud rate
 
+baud = 230400   # Baud rate
+devpath = '/dev/ttyUSB1'
 drive = 0.03    # Vrms, the drive amplitude
 fstart = 10      # Hz, the sweep begin frequency
 fend = 100e3     # Hz, the sweep end frequency
@@ -24,55 +25,55 @@ freqlist = logspace(log10(fstart),log10(fend),num=fpoints)
 datafile = gnupath + '\\' + testname + '.dat'
 term = '\r\n' # String terminator
 
-# getcgr() 
-# Returns an instrument variable for the cgr scope, or an error message
-# if the connection fails.
+""" getcgr() 
+    Returns an instrument variable for the cgr scope, or an error message
+    if the connection fails. """
 def getcgr():
     try:
-        cgr = visa.instrument('COM' + str(cgrch))
-        cgr.baud_rate = baud
-        cgr.term_chars = '\r\n' # Receive string termination
+        cgr = serial.Serial(devpath)
+        cgr.timeout = 1 # Timeout in seconds
+        cgr.baudrate = baud
         return cgr
     except:
-        print('Cannot connect to cgr-101 on COM' + str(cgrch))
+        print('Cannot connect to cgr-101 at ' + devpath)
 
-# sendcgr(instrument,command)
-# Send an ascii command string to the CGR scope
-def sendcgr(cgr,cmd):
-    cgr.write(cmd + term)
+""" sendcgr(handle,command)
+    Send an ascii command string to the CGR scope """
+def sendcgr(handle,cmd):
+    handle.write(cmd + term)
     time.sleep(0.1) # Don't know if there's a command buffer
     
-# askcgr(instrument,command)
-# Send an ascii command and return the reply
-def askcgr(cgr,cmd):
-    sendcgr(cgr,cmd)
+""" askcgr(handle,command)
+    Send an ascii command and return the reply """
+def askcgr(handle,cmd):
+    sendcgr(handle,cmd)
     try:
-        retstr = cgr.read()
+        retstr = handle.readline()
         return(retstr)
     except:
         return('No reply')
         
-# setamp(cgr,amplitude)
-# Set the output amplitude in volts
-def setamp(cgr,volts):
+""" setamp(handle,amplitude)
+    Set the output amplitude in volts """
+def setamp(handle,volts):
     if volts > 3:
         print('Illegal amplitude -- exceeds 3V')
-        sendcgr(cgr,'W A 255')
+        sendcgr(handle,'W A 255')
     else:
         azero = int(255 * float(volts)/3.0)
-        sendcgr(cgr,'W A ' + str(azero))
+        sendcgr(handle,'W A ' + str(azero))
         
-# setfreq(cgr,frequency)
-# Set the cgr output frequency in Hz
-def setfreq(cgr,freq):
+""" setfreq(handle,frequency)
+    Set the cgr output frequency in Hz """
+def setfreq(handle,freq):
     fres = 0.09313225746
     pval = int(freq/fres)
     freqstr = basecon(pval)
     sendstr = ('W F ' + freqstr)
-    sendcgr(cgr,sendstr)
+    sendcgr(handle,sendstr)
 
-# Converts a decimal number to a 32-bit base-256 number for setting the 
-# CGR frequency.
+""" Converts a decimal number to a 32-bit base-256 number for setting the 
+    CGR frequency. """
 def basecon(decimal):
     fthree = int(decimal/(2**24))
     ftwo = int((decimal%(2**24))/(2**16))
@@ -82,5 +83,4 @@ def basecon(decimal):
         str(fzero))
     return(retstr)   
 
-cgr = getcgr()
     
